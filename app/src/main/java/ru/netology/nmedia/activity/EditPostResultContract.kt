2b6ede7d27e6bf1fe7c8bb9127a52070f946b4f5
Andrei Razborov <andreyrd94@gmail.com>
2023-03-25 20:32:35 +0300

    «3.2. Организация навигации (перемещение между Activity)»

--- app/src/main/java/ru/netology/nmedia/activity/EditPostResultContract.kt
+++ app/src/main/java/ru/netology/nmedia/activity/EditPostResultContract.kt
@@ -9,7 +9,7 @@ import ru.netology.nmedia.dto.Post
 class EditPostResultContract : ActivityResultContract<Post, Post?>() {
 
     override fun createIntent(context: Context, input: Post): Intent {
-      val  intent =  Intent(context, EditPostActivity::class.java)
+        val intent = Intent(context, EditPostActivity::class.java)
         intent.putExtra("content", input.content)
         intent.putExtra("video", input.video)
         return intent
@@ -18,9 +18,11 @@ class EditPostResultContract : ActivityResultContract<Post, Post?>() {
     override fun parseResult(resultCode: Int, intent: Intent?): Post? =
         if (resultCode == Activity.RESULT_OK) {
             intent?.getStringExtra(Intent.EXTRA_TEXT)
-            Post(0,"",intent?.getStringExtra("content").toString(),"",
-                0,0,
-                false,intent?.getStringExtra("video").toString())
+            Post(
+                0, "", intent?.getStringExtra("content").toString(), "",
+                0, 0,
+                false, intent?.getStringExtra("video").toString()
+            )
         } else {
             null
         }
